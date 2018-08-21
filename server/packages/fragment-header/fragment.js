b8902b969adca047cd33829901fc9bcea1665e01
Justen Doherty <justen.doherty@kurtgeiger.com>
2018-08-21 14:31:24 +0100

    added fragment server as vars

--- server/packages/fragment-header/fragment.js
+++ server/packages/fragment-header/fragment.js
@@ -2,6 +2,7 @@ const http = require('http')
 const url = require('url')
 const fs = require('fs')
 const fragmentPort = 8082
+const fragmentServer = '34.245.40.163'
 
 const server = http.createServer((req, res) => {
   const pathname = url.parse(req.url).pathname
@@ -13,7 +14,7 @@ const server = http.createServer((req, res) => {
     default:
       res.writeHead(200, {
         'Content-Type': 'text/html',
-        'Link': `<http://34.245.40.163:${fragmentPort}/public/bundle.js>; rel="fragment-script"`
+        'Link': `<http://${fragmentServer}:${fragmentPort}/public/bundle.js>; rel="fragment-script"`
       })
       return res.end('')
   }
